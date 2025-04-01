f09faae8c4cfc06eb6218933e3c6319662dc0d39
William Murray <william@fstopify.com>
2025-04-01 08:48:31 +0000

    Update schema.sql to reflect current bank.db state after wipe

--- schema.sql
+++ schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE accounts (account_number TEXT PRIMARY KEY, balance REAL);
 CREATE TABLE transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, account_number TEXT, amount REAL, type TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
 CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT);
 INSERT INTO accounts VALUES ('1234', 375.50);
-INSERT INTO transactions (account_number, amount, type) VALUES ('1234', 50.0, 'deposit');
-INSERT INTO transactions (account_number, amount, type) VALUES ('1234', 50.0, 'deposit');
+INSERT INTO transactions (account_number, amount, type, timestamp) VALUES ('1234', 50.0, 'deposit', '2025-03-31 15:37:11');
+INSERT INTO transactions (account_number, amount, type, timestamp) VALUES ('1234', 50.0, 'deposit', '2025-03-31 15:53:56');
 INSERT INTO users VALUES ('testuser', 'password123');
